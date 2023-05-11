6ec16a9b7a9f945d4e426502b496ff578129da2d
Poojan <poojanbhalodiya02@gmail.com>
2023-05-11 23:54:27 +0530

    fire base crud

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDJww2FdWpgqmJhyKHFexn5hAHz8axv50o",
+  authDomain: "templating-e157d.firebaseapp.com",
+  databaseURL: "https://templating-e157d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "templating-e157d",
+  storageBucket: "templating-e157d.appspot.com",
+  messagingSenderId: "1024435896137",
+  appId: "1:1024435896137:web:d931338822f9b008dfa498",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
